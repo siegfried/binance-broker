298c9c45f71163ae5d5cc85df7aac2a5af8478f4
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2025-01-24 04:35:00 +0800

    Fix default.nix

--- default.nix
+++ default.nix
@@ -1,12 +1,36 @@
-(import (
-  let
-    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-    nodeName = lock.nodes.root.inputs.flake-compat;
-  in
-  fetchTarball {
-    url =
-      lock.nodes.${nodeName}.locked.url
-        or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.${nodeName}.locked.rev}.tar.gz";
-    sha256 = lock.nodes.${nodeName}.locked.narHash;
-  }
-) { src = ./.; }).defaultNix
+{
+  pkgs ? import <nixpkgs> { },
+}:
+let
+  pname = "binance-broker";
+  version = "0.1.0";
+  deps = pkgs.importNpmLock.buildNodeModules {
+    npmRoot = ./.;
+    nodejs = pkgs.nodejs;
+  };
+in
+pkgs.stdenv.mkDerivation {
+  inherit pname version;
+  src = ./.;
+
+  buildInputs = with pkgs; [
+    nodejs
+  ];
+
+  buildPhase = ''
+    rm -rf node_modules
+    ln -s ${deps}/node_modules .
+    npm run build
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    ln -s ${deps}/node_modules $out
+    cp -r .next $out
+    cp -r public $out
+    cp package.json $out
+    cp drizzle.config.ts $out
+    mkdir -p $out/src/db
+    cp src/db/schema.ts $out/src/db
+  '';
+}
