9e11813aabe2ee666bcd89657e71759f29e72c8e
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2025-01-26 05:06:35 +0800

    Add account filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 import { eq } from "drizzle-orm/sql";
 import { db } from "@/db";
 import { accountsTable, signalsTable } from "@/db/schema";
+import Link from "next/link";
+import { z } from "zod";
+import { desc } from "drizzle-orm/expressions";
 
-export default async function Home() {
-  const rows = await db.select().from(signalsTable).innerJoin(accountsTable, eq(signalsTable.accountId, accountsTable.id));
+type SignalViewProps = {
+  rows: {
+    signal: typeof signalsTable.$inferSelect,
+    account: typeof accountsTable.$inferSelect,
+  }[]
+};
+
+function SignalView(props: SignalViewProps) {
+  const rows = props.rows;
   return (
     <div className="border rounded-sm overflow-hidden">
       <table className="table-auto w-full">
-        <thead className="bg-slate-100">
+        <thead className="bg-slate-100 border-b">
           <tr>
             <th className="p-2">Symbol</th>
             <th className="p-2">Timestamp</th>
@@ -28,6 +38,48 @@ export default async function Home() {
           </tr>))}
         </tbody>
       </table>
+    </div>
+  )
+}
+
+function AccountFilterList(props: { accounts: typeof accountsTable.$inferSelect[], accountId?: number }) {
+  const { accounts, accountId } = props;
+  if (accounts.length <= 0) return;
+  return (
+    <div className="flex flex-rows space-x-2">
+      <Link className={`p-2 rounded-sm ${accountId && "bg-slate-100"}`} href={"/"}>All</Link>
+      {accounts.map((account) => (
+        <Link className={`p-2 rounded-sm ${accountId !== account.id && "bg-slate-100"}`}
+          href={`/?account_id=${account.id}`} key={account.id}>
+          {account.name}
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default async function Home({ searchParams }: { searchParams: Promise<{ account_id?: string }> }) {
+  const { account_id } = await searchParams;
+  const accountIdParsed = z.coerce.number().safeParse(account_id);
+  let rowsQuery = db
+    .select()
+    .from(accountsTable)
+    .innerJoin(signalsTable, eq(signalsTable.accountId, accountsTable.id))
+    .orderBy(desc(signalsTable.timestamp))
+    .$dynamic();
+  if (account_id) {
+    rowsQuery = rowsQuery.where(eq(accountsTable.id, parseInt(account_id)))
+  }
+  const rows = await rowsQuery;
+  const accounts = rows.reduce((acc, row) => {
+    const { account } = row;
+    acc.set(account.id, account);
+    return acc;
+  }, new Map<number, typeof accountsTable.$inferSelect>()).values();
+  return (
+    <div className="space-y-4">
+      <AccountFilterList accounts={Array.from(accounts)} accountId={accountIdParsed.data} />
+      <SignalView rows={rows} />
     </div>
   );
 }
