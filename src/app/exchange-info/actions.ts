1195e13d43d91c45df6ab88c2b5719ae220686d1
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2025-01-31 15:58:54 +0800

    Refactor

--- src/app/exchange-info/actions.ts
+++ src/app/exchange-info/actions.ts
@@ -1,5 +1,5 @@
 'use server'
 
-import { getExchangeInfo as _getExchangeInfo } from "@/binance/usdm";
+import { getExchangeInfo } from "@/binance/usdm";
 
-export const getExchangeInfo = _getExchangeInfo;
+export { getExchangeInfo };
