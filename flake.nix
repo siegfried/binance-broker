b580f850315fda77562272d0a33978eca5aa9b8a
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2025-02-06 12:37:03 +0800

    Remove flake-utils

--- flake.nix
+++ flake.nix
@@ -3,24 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
+    { self, nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
     {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        lib = pkgs.lib;
-        nodejs = pkgs.nodejs;
-      in
-      {
-        packages = {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          nodejs = pkgs.nodejs;
+          lib = pkgs.lib;
+        in
+        {
           default = pkgs.buildNpmPackage {
             pname = "binance-broker";
             version = "0.1.0";
@@ -57,11 +59,19 @@
               chmod +x $out/bin/start
             '';
           };
-        };
+        }
+      );
 
-        devShells.default = pkgs.mkShell {
-          packages = [ nodejs ];
-        };
-      }
-    );
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [ pkgs.nodejs ];
+          };
+        }
+      );
+    };
 }
